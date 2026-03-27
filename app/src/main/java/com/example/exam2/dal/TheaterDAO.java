75224c9b9560ef3389f35999be078953ae7e964d
Phung Hai Nam <nam0072004@gmail.com>
2026-03-27 11:23:13 +0700

    theater+ ticket dao

---
+++ app/src/main/java/com/example/exam2/dal/TheaterDAO.java
@@ -0,0 +1,17 @@
+package com.example.exam2.dal;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import com.example.exam2.entities.Theater;
+import java.util.List;
+@Dao
+public interface TheaterDAO {
+    @Query("SELECT * FROM theaters")
+    List<Theater> getAll();
+
+    @Query("SELECT * FROM theaters WHERE id = :id")
+    Theater getById(int id);
+
+    @Insert
+    void insert(Theater theater);
+}
